6a8b205cd27cc8f9c5d1c4c952ac98fb5d95d81c
mariangz <marianosgimenez@gmail.com>
2022-03-02 16:43:50 +0100

    add list component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import List from './List';
+
 export default function App({ name, technology }) {
   return (
-    <h1>
-      I'm {name} and I'm learning {technology}
-    </h1>
+    <>
+      <h1>
+        I'm {name} and I'm learning {technology}
+      </h1>
+      <List />
+    </>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,10 @@
+export default function List() {
+  return (
+    <ul>
+      <li>No Idea</li>
+      <li>Learning</li>
+      <li>Project</li>
+      <li>Ready :D </li>
+    </ul>
+  );
+}
