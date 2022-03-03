bbd91161389a7baff146087673b620c18a97b3e0
mariangz <marianosgimenez@gmail.com>
2022-03-03 14:51:55 +0100

    remove unnecessary files

--- src/List.js
+++ src/List.js
@@ -1,12 +1,15 @@
 import './index.css';
-
+import React from 'react';
 export default function List() {
   return (
-    <ul>
-      <li>No Idea</li>
-      <li>Learning</li>
-      <li>Project</li>
-      <li>Ready :D </li>
-    </ul>
+    <>
+      <ul>
+        <li>No Idea</li>
+        <li>Learning</li>
+        <li>Project</li>
+        <li>Ready :D </li>
+      </ul>
+      {React.createElement('p', {}, 'Hi, testing. This is not JSX')}
+    </>
   );
 }
