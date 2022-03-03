36bb52e1628b39aa4bbb8870500336536cfdf77f
mariangz <marianosgimenez@gmail.com>
2022-03-03 18:00:23 +0100

    add data file  and style components

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import Title from './Title';
 
 export default function Card({ title }) {
   return (
-    <>
+    <div className="card">
       <Title title={title} />
-    </>
+    </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+  {
+    id: 1,
+    title: 'Svelte',
+    description: "It's a framework",
+  },
+  {
+    id: 2,
+    title: 'Vue',
+    description: "It's a framework too",
+  },
+];
