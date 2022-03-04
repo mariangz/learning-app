cacb05633951407e577bf8a36f9daacc2612a572
mariangz <marianosgimenez@gmail.com>
2022-03-04 12:11:57 +0100

    add input to enter values

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import Card from './Card';
 import './index.css';
 import Container from './Container';
+import MainContainer from './MainContainer';
 
 export default function App() {
   return (
     <>
-      <Container theme="dark">
-        <Card title="No Idea" />
-        <Card title="Learning" />
-        <Card title="Project" />
-        <Card title="Ready" />
-      </Container>
+      <MainContainer>
+        <Container theme="dark">
+          <Card title="No Idea" />
+          <Card title="Learning" />
+          <Card title="Project" />
+          <Card title="Ready" />
+        </Container>
+        <Container theme="light">
+          <label className="label" htmlFor="inputTask">
+            Task
+          </label>
+          <input id="inputTask" />
+        </Container>
+      </MainContainer>
     </>
   );
 }
---
+++ src/MainContainer.js
@@ -0,0 +1,3 @@
+export default function Container({ children }) {
+  return <div className="main-container container">{children}</div>;
+}
