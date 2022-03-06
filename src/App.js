4f0acd363435a8f64acfe47a96650c03367e0b4e
Mariano <marianosgimenez@gmail.com>
2022-03-06 22:51:52 +0100

    add dataList first card

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import Card from './Card';
 import './index.css';
 import Container from './Container';
 import MainContainer from './MainContainer';
+import Task from './Task';
+import { data } from './data';
 
 export default function App() {
-  return (
-    <>
-      <MainContainer>
-        <Container theme="dark">
-          <Card title="No Idea" />
-          <Card title="Learning" />
-          <Card title="Project" />
-          <Card title="Ready" />
-        </Container>
-        <Container theme="light">
-          <label className="label" htmlFor="inputTask">
-            Task
-          </label>
-          <input id="inputTask" />
-        </Container>
-      </MainContainer>
-    </>
-  );
+	const dataList = data.map((item) => (
+		<Task title={item.title} description={item.description} />
+	));
+
+	return (
+		<>
+			<MainContainer>
+				<Container theme='dark'>
+					<Card title='No Idea' dataList={dataList} />
+					<Card title='Learning' />
+					<Card title='Project' />
+					<Card title='Ready' />
+				</Container>
+				<Container theme='light'>
+					<label className='label' htmlFor='inputTask'>
+						Task
+					</label>
+					<input id='inputTask' />
+				</Container>
+			</MainContainer>
+		</>
+	);
 }
--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,8 @@
-import Task from './Task';
-import { data } from './data';
-
-export default function Card({ title }) {
-  const dataList = data.map((item) => (
-    <Task title={item.title} description={item.description} />
-  ));
-  return (
-    <div className="card">
-      <h1>{title}</h1>
-      {dataList}
-    </div>
-  );
+export default function Card({ title, dataList }) {
+	return (
+		<div className='card'>
+			<h1>{title}</h1>
+			{dataList}
+		</div>
+	);
 }
