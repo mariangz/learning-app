8a1b21c3b2bbbb09a98eb108b50cfeb52475bf0c
Mariano <marianosgimenez@gmail.com>
2022-03-09 15:44:05 +0100

    add new component, improve styling

--- src/AddCardButton.js
+++ src/AddCardButton.js
@@ -1,7 +1,13 @@
 export default function AddCardButton() {
 	return (
 		<div className='AddCardButton__container'>
-			<button className='AddCardButton__btn'>+ Add a Card</button>
+			<input onC className='AddCardButton__input' />
+			<button
+				className='AddCardButton__btn'
+				onClick={() => console.log('haloo')}
+			>
+				+ Add a Card
+			</button>
 		</div>
 	);
 }
--- src/Column.js
+++ src/Column.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import AddCardButton from './AddCardButton';
 
 export default function Column({ title, dataList }) {
+	const [task, setTask] = useState(dataList);
+	const handleInputClick = () => {
+		const value = document.getElementsByClassName('AddCardButton__input').value;
+		console.log(value);
+	};
 	return (
 		<div className='column'>
 			<h1 className='column column__title'>{title}</h1>
-			<div className='column column__body'>{dataList}</div>
-			<AddCardButton />
+			<div className='column column__body'>{task}</div>
+			<div className='AddCardButton__container'>
+				<input onC className='AddCardButton__input' />
+				<button className='AddCardButton__btn' onClick={handleInputClick}>
+					+ Add a Card
+				</button>
+			</div>
 		</div>
 	);
 }
