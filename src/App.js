3e33f58bda5b6e37648ce0069834f07f8aee18fb
Mariano <marianosgimenez@gmail.com>
2022-03-14 16:52:08 +0100

    remove columns components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import Column from './Column';
+import { useState } from 'react';
 import './index.css';
-import Container from './Container';
+import AppForm from './AppForm';
+import Column from './Column';
 
 export default function App() {
+	const [tasks, setTasks] = useState([]);
+	const [entry, setEntry] = useState([]);
+	const [showButton, setShowButton] = useState(true);
+	const [showInput, setShowInput] = useState(false);
+
+	function handleFormSubmit(e) {
+		e.preventDefault();
+		setTasks([...tasks, entry]);
+		setEntry('');
+		setShowButton(true);
+		setShowInput(false);
+	}
+
+	function handleShowInputClick() {
+		setShowButton(false);
+		setShowInput(true);
+	}
+
+	function handleEntryChange(e) {
+		setEntry(e.target.value);
+	}
+
+	const tasksList = tasks.map((task) => <li key={task}>{task}</li>);
+
 	return (
-		<>
-			<Container theme='light'>
-				<Column title='No Idea' />
-				<Column title='Learning' />
-				<Column title='Project' />
-				<Column title='Ready' />
-			</Container>
-		</>
+		<div className='container'>
+			<Column title='No Idea' tasksList={tasksList}>
+				<AppForm
+					showInput={showInput}
+					entry={entry}
+					onFormSubmit={handleFormSubmit}
+					onShowInputClick={handleShowInputClick}
+					onEntryChange={handleEntryChange}
+				/>
+			</Column>
+		</div>
 	);
 }
--- src/Container.js
+++
@@ -1,3 +0,0 @@
-export default function Container({ children, theme }) {
-	return <div className={`container ${theme}`}>{children}</div>;
-}
