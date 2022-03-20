6924e3c5eb71710ff39c43ee453fed15c67c7f0a
Mariano <marianosgimenez@gmail.com>
2022-03-20 17:08:42 +0100

    style: change bgcolor form, add new font

--- src/AppForm.js
+++ src/AppForm.js
@@ -1,44 +1,44 @@
 export default function AppForm(props) {
-	const {
-		entry,
-		showInput,
-		onFormSubmit,
-		onShowInputClick,
-		onEntryChange,
-		id,
-		validation,
-	} = props;
-	let btn;
-	if (showInput[id]) {
-		btn = (
-			<>
-				<form
-					onSubmit={onFormSubmit}
-					className='AddCardButton__container'
-					id={id}
-				>
-					<input
-						onChange={onEntryChange}
-						placeholder='Task'
-						value={entry}
-						className='AddCardButton__input'
-						id={id}
-					/>
-					<p id={id} className='validation-msg'>
-						{validation}
-					</p>
-					<button type='submit' className='AddCardButton__btn' id={id}>
-						Enter
-					</button>
-				</form>
-			</>
-		);
-	} else {
-		btn = (
-			<button onClick={onShowInputClick} className='AddCardButton__btn' id={id}>
-				+ Add a Task
-			</button>
-		);
-	}
-	return btn;
+  const {
+    entry,
+    showInput,
+    onFormSubmit,
+    onShowInputClick,
+    onEntryChange,
+    id,
+    validation,
+  } = props;
+  let btn;
+  if (showInput[id]) {
+    btn = (
+      <>
+        <form
+          onSubmit={onFormSubmit}
+          className='AddCardButton__container'
+          id={id}
+        >
+          <input
+            onChange={onEntryChange}
+            placeholder='Task'
+            value={entry}
+            className='AddCardButton__input'
+            id={id}
+          />
+          <p id={id} className='validation-msg'>
+            {validation}
+          </p>
+          <button type='submit' className='AddCardButton__btn' id={id}>
+            Save
+          </button>
+        </form>
+      </>
+    );
+  } else {
+    btn = (
+      <button onClick={onShowInputClick} className='AddCardButton__btn' id={id}>
+        Add Task
+      </button>
+    );
+  }
+  return btn;
 }
