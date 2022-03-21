98b9ff18c87b028e412e6f273bbe0724c983df30
Mariano <marianosgimenez@gmail.com>
2022-03-21 16:36:44 +0100

    fea: entry state is an object

--- src/App.js
+++ src/App.js
@@ -8,39 +8,44 @@ import { useImmer } from 'use-immer';
 import produce from 'immer';
 
 export default function App() {
-  const [tasks, setTasks] = useState({
+  const [tasks, setTasks] = useImmer({
     'no-idea': [],
     learning: [],
     ready: [],
   });
 
-  const [entry, setEntry] = useState('');
+  const [entry, setEntry] = useState({
+    'no-idea': '',
+    learning: '',
+    ready: '',
+  });
   const [showButton, setShowButton] = useImmer({});
   const [showInput, setShowInput] = useImmer({});
   const [validation, setValidation] = useState({});
   console.log(entry);
+
   function handleFormSubmit(event) {
     const e = event.target;
     event.preventDefault();
-    if (!entry) {
-      setValidation(
-        produce(validation, (draft) => {
-          draft[e.id] = 'Enter a task';
-        })
-      );
-      return;
-    }
+    // if (!entry) {
+    //   setValidation(
+    //     produce(validation, (draft) => {
+    //       draft[e.id] = 'Enter a task';
+    //     })
+    //   );
+    //   return;
+    // }
     setTasks(
       produce(tasks, (draft) => {
         if (draft[e.id]) {
-          draft[e.id].push(entry);
+          draft[e.id].push(entry[e.id]);
         } else {
           draft[e.id] = [];
-          draft[e.id].push(entry);
+          draft[e.id].push(entry[e.id]);
         }
       })
     );
-    setEntry('');
+    setEntry({ ...entry, [e.id]: '' });
     setShowButton((draft) => {
       draft[e.id] = true;
     });
@@ -65,9 +70,8 @@ export default function App() {
 
   function handleEntryChange(event) {
     const e = event.target;
-    console.log(e);
 
-    setEntry(e.value);
+    setEntry({ ...entry, [e.id]: e.value });
   }
 
   function handleCancelClick(event) {
@@ -81,26 +85,28 @@ export default function App() {
     });
   }
 
-  // function handleNextClick() {
-  //   setTasks(
-  //     produce(tasks, (draft) => {
-  //       draft['learning'].push(item);
-  //       draft['no-idea'] = draft['no-idea'].filter((i) => i !== item);
-  //     })
-  //   );
-  // }
-
   return (
     <Container>
       <Column
         title='NO IDEA'
         tasksList={tasks['no-idea'].map((item) => (
-          <List key={item} item={item} />
+          <List
+            key={item}
+            item={item}
+            onNextClick={() =>
+              setTasks(
+                produce(tasks, (draft) => {
+                  draft['learning'].push(item);
+                  draft['no-idea'] = draft['no-idea'].filter((i) => i !== item);
+                })
+              )
+            }
+          />
         ))}
       >
         <AppForm
           showInput={showInput}
-          entry={entry}
+          entry={entry['no-idea']}
           onFormSubmit={handleFormSubmit}
           onShowInputClick={handleShowInputClick}
           onEntryChange={handleEntryChange}
@@ -112,12 +118,25 @@ export default function App() {
       <Column
         title='LEARNING'
         tasksList={tasks['learning'].map((item) => (
-          <List key={item} item={item} />
+          <List
+            key={item}
+            item={item}
+            onNextClick={() =>
+              setTasks(
+                produce(tasks, (draft) => {
+                  draft['ready'].push(item);
+                  draft['learning'] = draft['learning'].filter(
+                    (i) => i !== item
+                  );
+                })
+              )
+            }
+          />
         ))}
       >
         <AppForm
           showInput={showInput}
-          entry={entry}
+          entry={entry['learning']}
           onFormSubmit={handleFormSubmit}
           onShowInputClick={handleShowInputClick}
           onEntryChange={handleEntryChange}
@@ -129,12 +148,22 @@ export default function App() {
       <Column
         title='READY'
         tasksList={tasks['ready'].map((item) => (
-          <List key={item} item={item} />
+          <List
+            key={item}
+            item={item}
+            onNextClick={() =>
+              setTasks(
+                produce(tasks, (draft) => {
+                  draft['ready'] = draft['ready'].filter((i) => i !== item);
+                })
+              )
+            }
+          />
         ))}
       >
         <AppForm
           showInput={showInput}
-          entry={entry}
+          entry={entry['ready']}
           onFormSubmit={handleFormSubmit}
           onShowInputClick={handleShowInputClick}
           onEntryChange={handleEntryChange}
--- src/List.js
+++ src/List.js
@@ -1,12 +1,12 @@
 import next from './images/next.svg';
 
-export default function List({ item }) {
+export default function List({ item, onNextClick }) {
   return (
-    <li key={item}>
+    <li>
       <span className='line'></span>
       <div className='list-content'>
         {item}
-        <button className='next-btn'>
+        <button onClick={onNextClick} className='next-btn'>
           <img className='next-icon' src={next} alt='' />
         </button>
       </div>
