e2bdb23ba8f6bcc1fae08517b8839bc00009df3f
Mariano <marianosgimenez@gmail.com>
2022-03-23 15:27:06 +0100

    style: next and previous hidden

--- src/List.js
+++ src/List.js
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import next from './images/next.svg';
 import prev from './images/prev.svg';
 
@@ -5,19 +6,21 @@ export default function List({ item, onPrevClick, onNextClick, column, last }) {
   console.log(column);
   return (
     <li>
-      <span className='line'></span>
+      {/* <span className='line'></span> */}
       <div className='list-content'>
-        {column !== 0 && (
-          <button onClick={onPrevClick} className='move-btn'>
-            <img className='prev-icon' src={prev} alt='' />
-          </button>
-        )}
+        <button
+          onClick={onPrevClick}
+          className={clsx({ 'move-btn': true, hidden: column === 0 })}
+        >
+          <img className='prev-icon' src={prev} alt='' />
+        </button>
         {item}
-        {!last && (
-          <button onClick={onNextClick} className='move-btn'>
-            <img className='prev-icon' src={next} alt='' />
-          </button>
-        )}
+        <button
+          onClick={onNextClick}
+          className={clsx({ 'move-btn': true, hidden: last })}
+        >
+          <img className='prev-icon' src={next} alt='' />
+        </button>
       </div>
     </li>
   );
